2c17ec5e4ef67d91c46f88af5466be08b97a1c27
sonle <sonle1997@gmail.com>
2022-05-19 14:45:17 +0700

    init backend source

---
+++ jiro/jiro/src/test/java/com/sonle/jiro/JiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonle.jiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
